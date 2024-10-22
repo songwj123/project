83297fdaf18c3ccd99ae5f01ef2071edcb00b203
Administrator <15237378320@163.com>
2024-10-22 18:29:06 +0800

    更新部分代碼邏輯

--- GetAsinsApi.py
+++ GetAsinsApi.py
@@ -13,13 +13,27 @@ def get_asins(asin):
         data = res.json()
         title_list = []
         description_list = []
+        rank = []
         if data and "attributes" in data:
             title_list = [point["value"] for point in data["attributes"]["item_name"]]
             description_list = [point["value"] for point in data["attributes"]["bullet_point"]]
+        if data and "salesRanks" in data:
+            if isinstance(data["salesRanks"], dict) and "displayGroupRanks" in data["salesRanks"]:
+                for i in data["salesRanks"]["displayGroupRanks"]:
+                    rank.append(str(i["rank"]))
+            elif isinstance(data["salesRanks"], list):
+                for group in data["salesRanks"]:
+                    if "displayGroupRanks" in group:
+                        for i in group["displayGroupRanks"]:
+                            rank.append(str(i["rank"]))
+        title_str = " ".join(title_list)
+        description_str = " ".join(description_list)
+        rank_int = int(rank[0]) if rank else None
         r = {
             "asin": asin,
-            "title": title_list,
-            "description": description_list
+            "title": title_str,
+            "description": description_str,
+            "rank": rank_int
         }
         return json.dumps(r)
     else:
